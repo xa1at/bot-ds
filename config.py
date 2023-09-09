52e77294f7157d48be43cc79385b6b2fa42912c6
Alim <82275454+xa1at@users.noreply.github.com>
2023-09-09 22:52:47 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,1 @@
-settings = {
-	'TOKEN': 'ODMxNTk3MDMwNzIyNzY0ODIx.YHXjFg.MwbaMQ6hDql11wLRGs5Zq6foAik', # Получение токена - https://prnt.sc/uofhur
-	'NAME BOT': 'Bot Xa1at', # Имя вашего бота
-	'ID': '831597030722764821', # Получение ID - https://prnt.sc/uofik2
-	'PREFIX': '!', # Префикс
-	'OWNER':'xa1at#1343',    # ИЗМЕНЯЙТЕ НА СВОЙ NICK:ID (Пример: Toil#5605)
-	'OWNER NAME':'Xa1at' # ИМЯ ВЛАДЕЛЬЦА (Пример: Toil)
-}
+
